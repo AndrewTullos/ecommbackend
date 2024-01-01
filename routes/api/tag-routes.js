b13953301813feb6e77021cdfde0a67b6572ae08
Drew Tullos <tullos.andrew@gmail.com>
2024-01-01 12:35:05 -0600

    git ignore fix

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,7 +46,9 @@ router.post("/", async (req, res) => {
 });
 
 // update a tag's name by its `id` value
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+	
+});
 
 // delete on tag by its `id` value
 router.delete("/:id", async (req, res) => {
